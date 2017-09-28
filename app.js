f90b71b5929aab519d6c400e5daa05919ce1c96d
BReily89 <BrandonReily.br@gmail.com>
2017-09-28 17:34:54 -0400

    added mongoose router

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+require('dotenv').config();
+
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -5,10 +8,15 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
